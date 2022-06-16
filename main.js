beed408dd2645bf4ddc08092fe7cea1a9443680b
unicorngirl2010 <86110608+unicorngirl2010@users.noreply.github.com>
2022-06-16 12:54:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,16 +17,60 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UTwBw1d23/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_gcSzCyIq/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log('Loaded!');
 }
 
+function check() {
+    img = document, getElementById('captured_image');
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+
+        gesture = results[0].label;
+
+        toSpeak = "";
+
+        if (gesture == "Call Me Hand") {
+            toSpeak = "Call Me";
+            document.getElementById("result_object_gesture_icon").innerHTML = "Call Me Hand";
+        }
+        else if (gesture == "Heart Hands") {
+            toSpeak = "Heart";
+            document.getElementById("result_object_gesture_icon").innerHTML = "Heart Hands";
+        }
+        else if (gesture == "Crossed Fingers") {
+            toSpeak = ""Crossed";
+            document.getElementById("result_object_gesture_icon").innerHTML = "Crossed Fingers";
+        }
+        else if (gesture == "Thumbs Up") {
+            toSpeak = "Up";
+            document.getElementById("result_object_gesture_icon").innerHTML = "Thumbs Up";
+        }
+        else if (gesture == "Pinched Fingures") {
+            toSpeak = "Pinched";
+            document.getElementById("result_object_gesture_icon").innerHTML = "Pinched Fingures";
+        }
+        speak();
+    }
+}
+
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
     synth.speak(utterThis);
 }
